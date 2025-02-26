e941cffe4799c0413ed728e529a9ff24afedad07
jose-cazal-itti <jose.cazal@itti.cazal>
2025-02-26 10:50:56 -0300

    agregaomos boton de biometria

--- packages/auth_biometrics/lib/features/auth/presentation/widgets/biometric_button.dart
+++ packages/auth_biometrics/lib/features/auth/presentation/widgets/biometric_button.dart
@@ -1,10 +1,35 @@
+import 'package:auth_biometrics/features/auth/presentation/bloc/biometric_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BiometricButton extends StatelessWidget {
-  const BiometricButton({super.key});
+  const BiometricButton({super.key, required this.onSuccess});
+  final ValueChanged<String> onSuccess;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocBuilder<BiometricBloc, BiometricState>(
+      builder: (context, state) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              iconSize: 108,
+              icon: const Icon(Icons.fingerprint_outlined, color: Colors.blue),
+              color: Colors.blue,
+              tooltip: 'login con huella',
+              onPressed: () {
+                context.read<BiometricBloc>().add(
+                  BiometricEvent.loginBiometric(
+                    onSuccess: onSuccess,
+                    onError: () {},
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
